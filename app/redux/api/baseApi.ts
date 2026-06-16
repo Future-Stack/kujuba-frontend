63db1432dcef848e0d1a0ddc1d21c3663a647377
Romicha Parvin <romicha@softvence.com>
2026-06-16 09:28:10 +0600

    console add for login

--- app/redux/api/baseApi.ts
+++ app/redux/api/baseApi.ts
@@ -5,6 +5,7 @@ export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
      baseUrl: process.env.NEXT_PUBLIC_API_URL,
+     
 
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("access_token");
